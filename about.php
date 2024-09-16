c7544ed2f124fb4f5c22688fe17c04f3c6d60218
harsh-gupta-10 <harshgupta24716@gmail.com>
2024-09-16 20:41:50 +0530

    Refactor: Add new responsive navigation to about page

--- about.php
+++ about.php
@@ -7,6 +7,7 @@
     <title>About NADC</title>
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/about.css">
+    <link rel="stylesheet" href="styles/new-nav.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -15,7 +16,13 @@
 </head>
 
 <body>
-    
+<nav style="z-index: 1002; 
+            position:fixed;
+            width: 100%;
+            top: 0;
+            ">
+            <?php include 'new-responsive-nav.php'; ?>
+</nav>
     <section class="about-herosection">
         <div class="about-wrapper">
             <div class="about-hero-image">
