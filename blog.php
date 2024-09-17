2fa13dd50151ec559cdf7594c9cdbcd84e2a8faa
sahil1330 <sahilmane025@gmail.com>
2024-09-17 21:26:30 +0530

    Add footer styles to blog.php

--- blog.php
+++ blog.php
@@ -11,6 +11,7 @@
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <link rel="stylesheet" href="styles/new-nav.css">
     <link rel="stylesheet" href="styles/fonts.css">
+    <link rel="stylesheet" href="styles/footer.css">
     
     <style>
         /* Blog post card design */
