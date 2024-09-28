0d6bbf521d01742d0cdc50bbf3493e85e72df063
harsh-gupta-10 <harshgupta24716@gmail.com>
2024-09-29 00:29:51 +0530

    Refactor hero slider images: Remove unnecessary image and update image source

--- index.php
+++ index.php
@@ -124,9 +124,7 @@
         </div>
         <div class="hero-right-row4 carousel slide " id="carouselExampleSlidesOnly">
           <div class="hero-right-row4-box1 carousel-inner" data-bs-ride="carousel" data-aos="fade-left">
-            <div class="carousel-item active">
-              <img src="images/toothbrush.jpg" class="slideimg w-100" alt="images/toothbrush.jpg">
-            </div>
+            
             <div class="carousel-item">
               <img src="images/dr-imgs/dr-slider-1.webp" class="slideimg w-100" alt="images/toothbrush.jpg">
             </div>
